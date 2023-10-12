46522554a760e88e64944c9645402fb1d06b36d9
JohnsonMasino <johnsonmasino@gmail.com>
2023-10-12 15:34:05 +0000

    Query 1

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,5 +1,8 @@
--- this querry ranks country origin of bands, ordered by the number of (non-unique) fans.
-SELECT origin, SUM(fans) AS nb_fans
-	FROM metal_band
-	GROUP BY origin
-	ORDER BY nb_fans DESC;
+-- this querry creates a table with unique users
+DROP TABLE IF EXISTS users
+CREATE TABLE users (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	email VARCHAR(255) NOT NULL UNIQUE,
+	name VARCHAR(255),
+	country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
+);
